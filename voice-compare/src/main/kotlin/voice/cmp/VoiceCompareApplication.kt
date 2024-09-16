767d26dd41eebe27d11dbd4a0302cbab1e547217
sajjad <sajjadyami77@gmail.com>
2024-09-16 15:01:30 +0330

    [test] now have test for the same voice and get 100.0

--- voice-compare/src/main/kotlin/voice/cmp/VoiceCompareApplication.kt
+++ voice-compare/src/main/kotlin/voice/cmp/VoiceCompareApplication.kt
@@ -5,12 +5,15 @@ import org.springframework.boot.CommandLineRunner
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.context.annotation.Bean
+import voice.cmp.voiceservice.VoiceComparison
 
 @SpringBootApplication
 class VoiceCompareApplication {
     @Bean
-    fun run(voiceRecorder: VoiceRecorder) = CommandLineRunner {
-        voiceRecorder.recordVoice(5,"sajjad")
+    fun run(voiceComparison: VoiceComparison) = CommandLineRunner {
+        val path1: String = "E:\\spring\\voice-compare\\voice-compare\\src\\main\\resources\\testvoice1.wav"
+        val path2: String = "E:\\spring\\voice-compare\\voice-compare\\src\\main\\resources\\testvoice2.wav"
+        println(voiceComparison.compareVoice(path1, path2))
     }
 }
 
